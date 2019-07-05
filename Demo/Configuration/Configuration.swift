8a4de6f96c7f1e4f60204d36abb6705ff22413b3
wangyongyue <wangyouyue@kingbirdplus.com>
2019-07-05 16:41:55 +0800

    m

---
+++ Demo/Configuration/Configuration.swift
@@ -0,0 +1,13 @@
+//
+//  Configuration.swift
+//  Demo
+//
+//  Created by apple on 2019/7/5.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import UIKit
+
+//主体颜色
+let  themeColor = UIColor.red
+
